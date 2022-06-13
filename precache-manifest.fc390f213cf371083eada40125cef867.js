60aaabad18d64109843dc435e2bf698f13baad50
dato <dato.tchkadua.1@iliauni.edu.ge>
2022-06-13 23:28:30 +0400

    Updates

---
+++ precache-manifest.fc390f213cf371083eada40125cef867.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b502d808b6a03102870338412c0c41",
+    "url": "/Stripe/index.html"
+  },
+  {
+    "revision": "c7843b408dc9858583af",
+    "url": "/Stripe/static/css/main.cbf0016e.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/Stripe/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stripe/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7843b408dc9858583af",
+    "url": "/Stripe/static/js/main.d0a239ea.chunk.js"
+  },
+  {
+    "revision": "8df455e174d9c00403bb",
+    "url": "/Stripe/static/js/runtime-main.bf3d9ebe.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/Stripe/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/Stripe/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/Stripe/static/media/phone.58d7e3d6.svg"
+  }
+]);
